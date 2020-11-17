44ac4312e83d9bfa4b27a0f31639d4c9784cbd59
sinaioutlander <49360850+sinaioutlander@users.noreply.github.com>
2020-11-17 19:46:54 +1100

    3.0.1: Update BepInEx build for 5.4.1, remove Test Class inspection on start, update libs, remove commented references

--- src/ExplorerCore.cs
+++ src/ExplorerCore.cs
@@ -14,7 +14,7 @@ namespace UnityExplorer
     public class ExplorerCore
     {
         public const string NAME = "UnityExplorer";
-        public const string VERSION = "3.0.0";
+        public const string VERSION = "3.0.1";
         public const string AUTHOR = "Sinai";
         public const string GUID = "com.sinai.unityexplorer";
         public const string EXPLORER_FOLDER = @"Mods\UnityExplorer";
@@ -84,9 +84,6 @@ namespace UnityExplorer
                 {
                     UIManager.Init();
                     Log("Initialized UnityExplorer UI.");
-
-                    // temp debug
-                    InspectorManager.Instance.Inspect(Tests.TestClass.Instance);
                 }
                 catch (Exception e)
                 {
