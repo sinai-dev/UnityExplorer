7f6a4514e4f7940f27514dabdc5c37a086ce9604
Sinai <outlander.sinai@gmail.com>
2021-04-25 21:19:00 +1000

    Handle cases where InputSystem is present but not active

--- src/Core/Input/InputManager.cs
+++ src/Core/Input/InputManager.cs
@@ -40,25 +40,42 @@ namespace UnityExplorer.Core.Input
 
         public static void Init()
         {
+            InitHandler();
+
+            CursorUnlocker.Init();
+        }
+
+        private static void InitHandler()
+        {
             if (InputSystem.TKeyboard != null || (ReflectionUtility.LoadModule("Unity.InputSystem") && InputSystem.TKeyboard != null))
             {
-                m_inputModule = new InputSystem();
-                CurrentType = InputType.InputSystem;
+                try
+                {
+                    m_inputModule = new InputSystem();
+                    CurrentType = InputType.InputSystem;
+
+                    // make sure its working, the package may be present but not enabled.
+                    GetKeyDown(KeyCode.F5);
+
+                    return;
+                }
+                catch
+                {
+                    ExplorerCore.LogWarning("The InputSystem package was found but it does not seem to be working, defaulting to legacy Input...");
+                }
             }
-            else if (LegacyInput.TInput != null || (ReflectionUtility.LoadModule("UnityEngine.InputLegacyModule") && LegacyInput.TInput != null))
+
+            if (LegacyInput.TInput != null || (ReflectionUtility.LoadModule("UnityEngine.InputLegacyModule") && LegacyInput.TInput != null))
             {
                 m_inputModule = new LegacyInput();
                 CurrentType = InputType.Legacy;
+
+                return;
             }
 
-            if (m_inputModule == null)
-            {
-                ExplorerCore.LogWarning("Could not find any Input Module Type!");
-                m_inputModule = new NoInput();
-                CurrentType = InputType.None;
-            }
-
-            CursorUnlocker.Init();
+            ExplorerCore.LogWarning("Could not find any Input Module Type!");
+            m_inputModule = new NoInput();
+            CurrentType = InputType.None;
         }
     }
 }
