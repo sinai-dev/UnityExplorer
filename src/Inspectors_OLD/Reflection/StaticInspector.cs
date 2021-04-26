af3ee071436de4304342375d2bd53913c2955a85
Sinai <outlander.sinai@gmail.com>
2021-04-26 19:56:41 +1000

    Rewriting everything from scratch, added generic ObjectPool system

---
+++ src/Inspectors_OLD/Reflection/StaticInspector.cs
@@ -0,0 +1,14 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+
+//namespace UnityExplorer.Inspectors.Reflection
+//{
+//    public class StaticInspector : ReflectionInspector
+//    {
+//        public override string TabLabel => $" <color=cyan>[S]</color> {base.TabLabel}";
+
+//        public StaticInspector(Type type) : base(type) { }
+//    }
+//}
