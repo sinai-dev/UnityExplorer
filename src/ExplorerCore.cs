eb221bd868cf879f6a4e2086635a7c440ac80856
Sinai <outlander.sinai@gmail.com>
2021-07-02 18:45:09 +1000

    Bump version

--- src/ExplorerCore.cs
+++ src/ExplorerCore.cs
@@ -20,7 +20,7 @@ namespace UnityExplorer
     public static class ExplorerCore
     {
         public const string NAME = "UnityExplorer";
-        public const string VERSION = "4.1.7";
+        public const string VERSION = "4.1.8";
         public const string AUTHOR = "Sinai";
         public const string GUID = "com.sinai.unityexplorer";
 
