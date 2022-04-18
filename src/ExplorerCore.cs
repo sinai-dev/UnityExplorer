70d67f1dadec6c5107ebc85fe5350e73ed3b3437
Sinai <49360850+sinai-dev@users.noreply.github.com>
2022-04-18 21:39:21 +1000

    Bump version

--- src/ExplorerCore.cs
+++ src/ExplorerCore.cs
@@ -14,7 +14,7 @@ namespace UnityExplorer
     public static class ExplorerCore
     {
         public const string NAME = "UnityExplorer";
-        public const string VERSION = "4.7.2";
+        public const string VERSION = "4.7.3";
         public const string AUTHOR = "Sinai";
         public const string GUID = "com.sinai.unityexplorer";
 
