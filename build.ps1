e1f3d6a2b74345a5f17c198cdd99476601dfb6e7
yukieiji <yuki31gakki97@yahoo.co.jp>
2023-03-10 01:58:40 +0900

    feat:BepInEx Unity IL2CPP CoreCLR build scripts

--- build.ps1
+++ build.ps1
@@ -90,6 +90,27 @@ Move-Item -Path $Path/UniverseLib.IL2CPP.Interop.dll -Destination $Path/plugins/
 Remove-Item $Path/../UnityExplorer.BepInEx.IL2CPP.CoreCLR.zip -ErrorAction SilentlyContinue
 7z a $Path/../UnityExplorer.BepInEx.IL2CPP.CoreCLR.zip .\$Path\*
 
+# ----------- BepInEx Unity IL2CPP CoreCLR -----------
+dotnet build src/UnityExplorer.sln -c BIE_Unity_Cpp_CoreCLR
+$Path = "Release/UnityExplorer.BepInEx.Unity.IL2CPP.CoreCLR"
+# ILRepack
+lib/ILRepack.exe /target:library /lib:lib/net472 /lib:lib/net6/ /lib:lib/interop/ /lib:$Path /internalize /out:$Path/UnityExplorer.BIE.Unity.IL2CPP.CoreCLR.dll $Path/UnityExplorer.BIE.Unity.IL2CPP.CoreCLR.dll $Path/mcs.dll $Path/Tomlet.dll
+# (cleanup and move files)
+Remove-Item $Path/Tomlet.dll
+Remove-Item $Path/mcs.dll
+Remove-Item $Path/Iced.dll
+Remove-Item $Path/Il2CppInterop.Common.dll
+Remove-Item $Path/Il2CppInterop.Runtime.dll
+Remove-Item $Path/Microsoft.Extensions.Logging.Abstractions.dll
+Remove-Item $Path/UnityExplorer.BIE.Unity.IL2CPP.CoreCLR.deps.json
+New-Item -Path "$Path" -Name "plugins" -ItemType "directory" -Force
+New-Item -Path "$Path" -Name "plugins/sinai-dev-UnityExplorer" -ItemType "directory" -Force
+Move-Item -Path $Path/UnityExplorer.BIE.Unity.IL2CPP.CoreCLR.dll -Destination $Path/plugins/sinai-dev-UnityExplorer -Force
+Move-Item -Path $Path/UniverseLib.IL2CPP.Interop.dll -Destination $Path/plugins/sinai-dev-UnityExplorer -Force
+# (create zip archive)
+Remove-Item $Path/../UnityExplorer.BepInEx.Unity.IL2CPP.CoreCLR.zip -ErrorAction SilentlyContinue
+7z a $Path/../UnityExplorer.BepInEx.Unity.IL2CPP.CoreCLR.zip .\$Path\*
+
 # ----------- BepInEx 5 Mono -----------
 dotnet build src/UnityExplorer.sln -c Release_BIE5_Mono
 $Path = "Release/UnityExplorer.BepInEx5.Mono"
